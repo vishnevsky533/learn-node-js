dbb587fb5686b90892db0964555186f0dbe127a2
vishnevsky533 <vishnevsky533@gmail.com>
2020-10-07 01:51:34 +0300

    part2 video5

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+
 const app = express();
 
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.static('public'));
 app.get('/',(req, res)=>{
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    res.render('index')
 })
 
 app.get('/about',(req, res)=>{
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+    res.render('about')
 })
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000...');
